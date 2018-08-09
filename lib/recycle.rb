4568ca2b6809b6c568d07d06b001dfaaf1024683
mallorymaher <mallory.maher@comcast.net>
2018-08-09 14:51:35 +0000

    Done.

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -13,21 +13,45 @@
 #   "latitude"=>"40.890848989",
 #   "park_site_name"=>"227th St. Plgd"
 # }
-
+ def address(hash)
+   return hash["address"]
+  end
 
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
 
+def borough(hash)
+  return hash["borough"]
+end
+
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
 
+def all_keys(hash)
+    hash.each_key do |key|
+    puts key
+  end
+end
+  
 #3. Define a method called `all_keys` that takes a hash as an argument.
 #This method should print out each key in the hash. 
 #You should use the `each_key` method to iterate over your hash and print out each key.
 
+def all_data(hash)
+    hash.each_value do |value|
+    puts value
+  end
+end
+
 #4. Define a method called `all_data` that takes a hash as an argument.
 #This method should print out each value in the hash.
 #You should use the `each_value` method to iterate over your hash and print out each value.
+
+def key_and_data(hash)
+    hash.each do |key, value|
+    puts "The #{key} is #{value}"
+  end
+end
 
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
